17b5fe90ef81aaa653f1d263792b8c99992cf10e
Mikolaj <mikolaj.urbanek@gmail.com>
2020-04-09 11:04:37 +0200

    PVP is fully playable with joy!

--- Engine.java
+++ Engine.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Engine {
     private Player player1;
     private Player player2;
 
-
     public static ArrayList<String> listOfPlayers = new ArrayList<>();
 
-    Engine (boolean isHuman1, boolean isHuman2) {
+    Engine(boolean isHuman1, boolean isHuman2) {
         this.player1 = new Player(isHuman1);
         this.player2 = new Player(isHuman2);
     }
+
     public Player getPlayerOne() {
         return this.player1;
     }
@@ -20,24 +21,31 @@ public class Engine {
         return this.player2;
     }
 
-
-    void shooting (Player gamer, Player opponent){
+    void shooting(Player gamer, Player opponent) {
+        Common.clearScreen();
+        System.out.println(String.format("~~~~|%s's board|~~~~", opponent.getPlayerName()));
         System.out.println(gamer.getShotsBoard());
+        System.out.println("~~~~|yours board|~~~~");
+        System.out.println(gamer.getPlayerBoard());
         System.out.println(String.format("now %s is shooting. Shoot! eg. G4", gamer.getPlayerName()));
         String userPosition = Common.getUserPosition("Enter shooting coordinates, for example G4.");
         char userLetter = userPosition.charAt(0);
         int userNumber = Integer.parseInt(userPosition.substring(1));
         int posY = Common.letterToNumber(userLetter) - 1;
         int posX = userNumber - 1;
-            if (opponent.getPlayerBoard().getSquare(posY, posX).getStatus() == "SHIP"){
-                    gamer.getShotsBoard().getSquare(posY, posX).setStatus("HIT");
-                    System.out.println("You have shot the enemy!");
+        if (opponent.getPlayerBoard().getSquare(posY, posX).getStatus() == "SHIP") {
+            gamer.getShotsBoard().getSquare(posY, posX).setStatus("HIT");
+            Common.clearScreen();
+            System.out.println("You have shot the enemy!");
 
-            }
-            else{gamer.getShotsBoard().getSquare(posY, posX).setStatus("MISSED");
-                 System.out.println("You have missed the ship");
+        } else {
+            gamer.getShotsBoard().getSquare(posY, posX).setStatus("MISSED");
+            Common.clearScreen();
+            System.out.println("You have missed the ship");
         }
         System.out.println(gamer.getShotsBoard());
+        Common.wait(2000);
+
     }
 
 
